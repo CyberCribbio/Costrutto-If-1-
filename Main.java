d149d3ea08e29b316da5db879759db065ed2be54
Cristian Valenti <122291593+CyberCribbio@users.noreply.github.com>
2023-01-28 16:33:19 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = ("Silvio");
+        int numbersOfLetter = 6;
+
+        if(numbersOfLetter != 6){
+            System.out.println("Il numero di lettere è 6");
+        }else{
+            System.out.println("Bravo, sai contare!");
+
+        }
+
+    }
+}
